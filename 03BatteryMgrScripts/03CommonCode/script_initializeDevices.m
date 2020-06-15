aa15e823d0b02a5222afd2b40d66dc9ee502c5a2
Joseph O <36710290+josephojo@users.noreply.github.com>
2020-06-14 22:42:25 -0400

    Fixing error due to the inability to find eloadArgs
    While Checking for the args of each device in script_initializeDevices, the program threw an errorCode of BAD_DEV_ARG thinking that the individual arguments were bad instead of just the group of arguments unavailable since they were not being used. Corrected the if statements to first check if args are available, if not skip device initialization. If args is not empty, and the arg needed is not available only then should error be caught.

--- 03BatteryMgrScripts/03CommonCode/script_initializeDevices.m
+++ 03BatteryMgrScripts/03CommonCode/script_initializeDevices.m
@@ -4,8 +4,8 @@ if strcmpi(caller, "gui")
     
     %ARRAY ELOAD
     %##########################################################################
-    if exist('eloadArgs', 'var')
-        if ~isempty(eloadArgs) && strcmpi(eloadArgs.type, "serial")
+    if exist('eloadArgs', 'var') && ~isempty(eloadArgs)
+        if strcmpi(eloadArgs.type, "serial")
             if ~exist('eload','var') ...
                     || (isvalid(eload) && strcmpi(eload.serialStatus(), "Disconnected"))
                 if strcmpi(eloadArgs.devName, "array")
@@ -28,8 +28,8 @@ if strcmpi(caller, "gui")
     %--------------------------------------------------------------------------
     %APM POWER SUPPLY
     %##########################################################################
-    if exist('psuArgs', 'var')
-        if ~isempty(psuArgs) && strcmpi(psuArgs.type, "serial")
+    if exist('psuArgs', 'var') && ~isempty(psuArgs)
+        if strcmpi(psuArgs.type, "serial")
             if ~exist('psu','var') ...
                     || (isvalid(eload) && strcmpi(psu.serialStatus(), "Disconnected"))
                 if strcmpi(psuArgs.devName, "array")
@@ -52,81 +52,81 @@ if strcmpi(caller, "gui")
     
     %THERMOCOUPLE MODULE
     %##########################################################################
-    if exist('tempModArgs', 'var')
-        if ~isempty(tempModArgs)
-            if tempModArgs.type == "modbus"
-                thermo = modbus('serialrtu',tempModArgs.COMPort,...
-                    'Timeout',thermoArgs.timeout); % Initializes a Modbus
-                %protocol object using serial RTU interface connecting to COM6 and a Time
-                %out of 10s.
-                thermo.BaudRate = tempModArgs.baudRate;
-                
-            elseif tempModArgs.type == "serial"
-                thermo = serialport(tempModArgs.COMPort, tempModArgs.baudRate);
-                thermo.StopBits = tempModArgs.stopBits;
-                thermo.ByteOrder = tempModArgs.byteOrder;
-                thermo.Terminator = tempModArgs.terminator;
-                thermo.Timeout = tempModArgs.timeout;
-            else
-                err.code = ErrorCode.BAD_DEV_ARG;
-                err.msg = "No connection type selected for the Temp. Module.";
-                send(errorQ, err);
-            end
+    if exist('tempModArgs', 'var') && ~isempty(tempModArgs)
+        if tempModArgs.type == "modbus"
+            thermo = modbus('serialrtu',tempModArgs.COMPort,...
+                'Timeout',thermoArgs.timeout); % Initializes a Modbus
+            %protocol object using serial RTU interface connecting to COM6 and a Time
+            %out of 10s.
+            thermo.BaudRate = tempModArgs.baudRate;
+            
+        elseif tempModArgs.type == "serial"
+            thermo = serialport(tempModArgs.COMPort, tempModArgs.baudRate);
+            thermo.StopBits = tempModArgs.stopBits;
+            thermo.ByteOrder = tempModArgs.byteOrder;
+            thermo.Terminator = tempModArgs.terminator;
+            thermo.Timeout = tempModArgs.timeout;
+        else
+            err.code = ErrorCode.BAD_DEV_ARG;
+            err.msg = "No connection type selected for the Temp. Module.";
+            send(errorQ, err);
         end
     end
     %--------------------------------------------------------------------------
     
     % LABJACK
     %##########################################################################
-    LJRelayPins = sysMCUArgs.relayPins;
-    
-    if strcmpi(sysMCUArgs.devName, "LJMCU")
-        if ~exist('ljasm','var')
-            ljasm = NET.addAssembly('LJUDDotNet');
-            ljudObj = LabJack.LabJackUD.LJUD;
-
-            % Open the first found LabJack U3.
-            [ljerror, ljhandle] = ljudObj.OpenLabJackS('LJ_dtU3', 'LJ_ctUSB', '0', ...
-                true, 0);
-
-            % Constant values used in the loop.
-            LJ_ioGET_AIN = ljudObj.StringToConstant('LJ_ioGET_AIN');
-            LJ_ioGET_AIN_DIFF = ljudObj.StringToConstant('LJ_ioGET_AIN_DIFF');
-            LJE_NO_MORE_DATA_AVAILABLE = ljudObj.StringToConstant('LJE_NO_MORE_DATA_AVAILABLE');
-
-            % Start by using the pin_configuration_reset IOType so that all pin
-            % assignments are in the factory default condition.
-            ljudObj.ePutS(ljhandle, 'LJ_ioPIN_CONFIGURATION_RESET', 0, 0, 0);
+    if exist('sysMCUArgs', 'var') && ~isempty(sysMCUArgs)
+        if strcmpi(sysMCUArgs.devName, "LJMCU")
+            LJRelayPins = sysMCUArgs.relayPins;
             
-            % Enable measurement pins as analog pins
-            ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.currMeasPins(1), 1, 0);
-            ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.currMeasPins(2), 1, 0);
-            ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.voltMeasPins(1), 1, 0);
-            ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.voltMeasPins(2), 1, 0);
-        end
-    elseif strcmpi(sysMCUArgs.devName, "ArdMCU")
+            if ~exist('ljasm','var')
+                ljasm = NET.addAssembly('LJUDDotNet');
+                ljudObj = LabJack.LabJackUD.LJUD;
+                
+                % Open the first found LabJack U3.
+                [ljerror, ljhandle] = ljudObj.OpenLabJackS('LJ_dtU3', 'LJ_ctUSB', '0', ...
+                    true, 0);
+                
+                % Constant values used in the loop.
+                LJ_ioGET_AIN = ljudObj.StringToConstant('LJ_ioGET_AIN');
+                LJ_ioGET_AIN_DIFF = ljudObj.StringToConstant('LJ_ioGET_AIN_DIFF');
+                LJE_NO_MORE_DATA_AVAILABLE = ljudObj.StringToConstant('LJE_NO_MORE_DATA_AVAILABLE');
+                
+                % Start by using the pin_configuration_reset IOType so that all pin
+                % assignments are in the factory default condition.
+                ljudObj.ePutS(ljhandle, 'LJ_ioPIN_CONFIGURATION_RESET', 0, 0, 0);
+                
+                % Enable measurement pins as analog pins
+                ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.currMeasPins(1), 1, 0);
+                ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.currMeasPins(2), 1, 0);
+                ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.voltMeasPins(1), 1, 0);
+                ljudObj.ePutS(ljhandle, 'LJ_ioPUT_ANALOG_ENABLE_BIT', sysMCUArgs.voltMeasPins(2), 1, 0);
+            end
+        elseif strcmpi(sysMCUArgs.devName, "ArdMCU")
             err.code = ErrorCode.FEATURE_UNAVAIL;
             err.msg = "Using the arduino relay for switching and measurements has not yet been implemented.";
             send(errorQ, err);
-        
-        % Arduino
-        %##########################################################################
-%         ardPort = 'COM8';
-%         ardSerial = instrfind('Port',ardPort, 'Status', 'open');
-%         if isempty(ardSerial)
-%             ard = serial(ardPort);
-%             ard.BaudRate = 115200;
-%             ard.DataBits = 8;
-%             ard.Parity = 'none';
-%             ard.StopBits = 1;
-%             ard.Terminator = 'LF';
-%             fopen(ard);
-%         elseif ~exist('ard','var')
-%             ard = ardSerial;
-%         end
-%         wait(2);
-        % -------------------------------------------------------------------------
-    
+            
+            % Arduino
+            %##########################################################################
+            %         ardPort = 'COM8';
+            %         ardSerial = instrfind('Port',ardPort, 'Status', 'open');
+            %         if isempty(ardSerial)
+            %             ard = serial(ardPort);
+            %             ard.BaudRate = 115200;
+            %             ard.DataBits = 8;
+            %             ard.Parity = 'none';
+            %             ard.StopBits = 1;
+            %             ard.Terminator = 'LF';
+            %             fopen(ard);
+            %         elseif ~exist('ard','var')
+            %             ard = ardSerial;
+            %         end
+            %         wait(2);
+            % -------------------------------------------------------------------------
+            
+        end
     end
     
 elseif strcmpi(caller, "cmdWindow")
