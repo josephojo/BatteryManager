c9a7d0c73de5a1ebe1cca2614d34026bbecd8362
Joseph O <36710290+josephojo@users.noreply.github.com>
2020-07-15 17:22:57 -0400

    Made Improvements to voc_vs_soc test file
    - Modified the OCV Data Script to be used for any cell that is available in the "007BatteryParam" file
    - Changed charge and discharge test to end when required voltage is reached (CC) instead of CC-CV used in charging
    - Increased wait time to 30 mins.
    - Removed code to save voltage and soc since there is a new script that can be run ("extractOCV") to do that.

--- 03BatteryMgrScripts/01SingleProfileScripts/voc_vs_soc.m
+++ 03BatteryMgrScripts/01SingleProfileScripts/voc_vs_soc.m
@@ -1,7 +1,9 @@
 
-% validateSOC; %Run Script to validate SOC value to have a point to start SOC estimation
-% 
+% voc_vs_soc; %Run Script to collect OCV and SOC signals for Equivalent
+% Circuit Models
+%  Test should be done in about 25°C temp
 
+%%
 currFilePath = mfilename('fullpath');
 % Seperates the path directory and the filename
 [path, filename, ~] = fileparts(currFilePath);
@@ -10,37 +12,50 @@ newStr = extractBetween(path,"",...
                "03DataGen","Boundaries","inclusive");
 dataLocation = newStr + "\01CommonDataForBattery\";
 
+
+%% Parameters
+
+% Load Table with cell information
+load(dataLocation + "007BatteryParam.mat", 'batteryParam')
+
+% Set specific test data
+cellID = "AB1";
+testSettings.tempChnls = [9, 10];
+waitTime = 1800;
+
+chargeVolt = batteryParam.chargedVolt(cellID);
+dischargeVolt = batteryParam.dischargedVolt(cellID);
+
 cRate = 1/30; % Run at C/30
-ratedCap = 3.178; % Ah
+ratedCap = batteryParam.capacity(cellID); % Ah
 chargeCurr = cRate * ratedCap;
 dischargeCurr = -(cRate * ratedCap);
-testSettings.tempChnls = [9, 10];
 
+%% Begin Test
+% Bring the cell to ful charge.
+battTS = chargeToSOC(1, 0.3*ratedCap, 'cellIDs', cellID, 'testSettings', testSettings);
 
-% Charge at about 25°C
-battTS = chargeToSOC(1, 0.3*ratedCap, 'cellIDs', "AB1", 'testSettings', testSettings);
-
-battTS_Wait_Chrg = waitTillTime(1200, 'cellIDs', "AB1", 'testSettings', testSettings);
+battTS_Wait_Chrg = waitTillTime(waitTime, 'cellIDs', cellID, 'testSettings', testSettings);
 battTS = appendBattTS2TS(battTS, battTS_Wait_Chrg);
 
 % Discharge at same (more or less constant temperature)
-battTS_dchrg = dischargeToSOC(0, dischargeCurr,  'cellIDs', "AB1",...
-    'testSettings', testSettings); % Discharge to 0% SOC
+battTS_dchrg = dischargeToVolt(dischargeVolt, dischargeCurr,  'cellIDs', cellID,...
+    'testSettings', testSettings); % Discharge to lowest voltage 
 battTS = appendBattTS2TS(battTS, battTS_dchrg);
-save(dataLocation + "008OCV_AB1.mat",'battTS', 'battTS_dchrg'); % ,'-append');
+save(dataLocation + "008OCV_" + cellID + ".mat",'battTS', 'battTS_dchrg'); % ,'-append');
 
-battTS_Wait_dchrg = waitTillTime(1200, 'cellIDs', "AB1", 'testSettings', testSettings);
+battTS_Wait_dchrg = waitTillTime(waitTime, 'cellIDs', cellID, 'testSettings', testSettings);
 battTS = appendBattTS2TS(battTS, battTS_Wait_dchrg);
-save(dataLocation + "008OCV_AB1.mat",'battTS', 'battTS_Wait_dchrg','-append');
+save(dataLocation + "008OCV_" + cellID + ".mat",'battTS', 'battTS_Wait_dchrg','-append');
 
 % Charge at same (more or less constant temperature)
-battTS_chrg = chargeToSOC(1, chargeCurr,  'cellIDs', "AB1",...
+battTS_chrg = chargeToVolt(chargeVolt, chargeCurr,  'cellIDs', cellID,...
     'testSettings', testSettings);
 battTS = appendBattTS2TS(battTS, battTS_chrg);
-save(dataLocation + "008OCV_AB1.mat",'battTS', 'battTS_chrg','-append');
+save(dataLocation + "008OCV_" + cellID + ".mat",'battTS', 'battTS_chrg','-append');
 
 
-ocv_OCV = battTS.Data(:,1);
-ocv_SOC = battTS.Data(:,3);
+% ocv_OCV = battTS.Data(:,1);
+% ocv_SOC = battTS.Data(:,3);
 
-save(dataLocation + "008OCV_AB1.mat", 'ocv_OCV', 'ocv_SOC','-append');
+% save(dataLocation + "008OCV_" + cellID + ".mat", 'ocv_OCV', 'ocv_SOC','-append');
