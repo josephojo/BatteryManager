de5cce456d21f3016fd7b6af75d97484fe38fc32
Joseph O <36710290+josephojo@users.noreply.github.com>
2020-07-16 13:10:59 -0400

    Created updateExpLogs.m
    Create this function so that code can be reused in other scripts

---
+++ 03BatteryMgrScripts/03CommonCode/updateExpLogs.m
@@ -0,0 +1,40 @@
+function updateExpLogs(Filename, Purpose, cellIDs, capacityLeft, batteryParam)
+% updateExpLogs Update Experiment Logs File
+%%
+currFilePath = mfilename('fullpath');
+% Seperates the path directory and the filename
+[path, ~, ~] = fileparts(currFilePath);
+
+saveLocation = extractBetween(path,"",...
+    "00BattManager","Boundaries","inclusive");
+
+%%
+
+DateCompleted = string(datestr(now,'yymmdd'));
+TimeCompleted = string(datestr(now,'HHMM'));
+
+T = readtable(saveLocation + "ExperimentLogs.xlsx");
+
+BatteryChem = batteryParam.chemistry(cellIDs(1));
+
+numCells = length(cellIDs);
+
+CellIDs = ""; CapacityLeft = "";
+for i = 1:numCells
+    if i ~= 1
+        CellIDs = CellIDs + ", ";
+        CapacityLeft = CapacityLeft + ", ";
+    end
+    CellIDs = CellIDs + cellIDs(i);
+    CapacityLeft = CapacityLeft + capacityLeft(i);
+end
+Tnew = table(Purpose,Filename,CellIDs,DateCompleted,TimeCompleted,...
+    BatteryChem, CapacityLeft);
+
+T = [T;Tnew];
+
+writetable(T, saveLocation + "ExperimentLogs.xlsx",'Sheet',1,'Range','A1')
+
+disp("Experiment Log Updated");
+
+end
