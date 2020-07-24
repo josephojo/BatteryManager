cef2908719f06618b76b306d5cd80a65ac8c599c
Joseph O <36710290+josephojo@users.noreply.github.com>
2020-07-24 00:40:31 -0400

    Ah Count for cell "AB4"

--- 03BatteryMgrScripts/03CommonCode/AhCounter.m
+++ 03BatteryMgrScripts/03CommonCode/AhCounter.m
@@ -121,16 +121,16 @@ try
         testSettings.cellConfig = 'single';
     end
     
-%     % Step 1: Bring the cell  to Full Capacity
-%     battTS_chrg = chargeToSOC(1, cRate_chrg*cap, 'cellIDs', cellIDs, 'testSettings', testSettings);
-% 
-%     % Step 2: Let the battery rest
-%     battTS_Wait_Chrg = waitTillTime(waitTime, 'cellIDs', cellIDs, 'testSettings', testSettings);
-%     battTS_temp = appendBattTS2TS(battTS_chrg, battTS_Wait_Chrg);
+    % Step 1: Bring the cell  to Full Capacity
+    battTS_chrg = chargeToSOC(1, cRate_chrg*cap, 'cellIDs', cellIDs, 'testSettings', testSettings);
+
+    % Step 2: Let the battery rest
+    battTS_Wait_Chrg = waitTillTime(waitTime, 'cellIDs', cellIDs, 'testSettings', testSettings);
+    battTS_temp = appendBattTS2TS(battTS_chrg, battTS_Wait_Chrg);
 
     % Step 3: Discharge to empty
     dischargeToEmpty; % there is an internal variable "battTS" in script
-%     battTS = appendBattTS2TS(battTS_temp, battTS);
+    battTS = appendBattTS2TS(battTS_temp, battTS);
 
     
     % Plot the data if true
