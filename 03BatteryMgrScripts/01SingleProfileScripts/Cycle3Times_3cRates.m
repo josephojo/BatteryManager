7a8d20ede72709ed261804dcefbbc7615550bb83
Joseph O <36710290+josephojo@users.noreply.github.com>
2020-08-15 14:25:19 -0400

    Ran the Cycle3Times_3cRates Script and got some data for model parameter estimation

--- 03BatteryMgrScripts/01SingleProfileScripts/Cycle3Times_3cRates.m
+++ 03BatteryMgrScripts/01SingleProfileScripts/Cycle3Times_3cRates.m
@@ -1,4 +1,10 @@
-cellIDs = "AB6";
+cellIDs = "AB1";
+
+waitTime = 900; % wait time for cool down periods in seconds (15 mins)
+if ~exist('caller', 'var')
+    caller = "cmdWindow";
+end
+
 testSettings.cellConfig = "single";
 testSettings.tempChnls = [9, 10];
 
@@ -18,31 +24,95 @@ metadata.startTime = string(datetime('now', 'Format','HHmm'));
 
 cRates = [0.4, 0.3;...
           1.5, 1.0;...
-          4.0, 3.0]; % [Discharge, Charge]
+          3.0, 2.5]; % [Discharge, Charge]
 
-save(dataLocation + "001Cycle3Times_"+dateStr,...
-    "cRates", "CAP", "metadata");
+% save(dataLocation + "001Cycle3Times_"+dateStr,...
+%     "cRates", "CAP", "metadata");
 
 %% Cycle 1
+% Discharge Stage
+msg = newline + "Cycle 1: Discharging to 0%. NextUp: Cycle 1 Cool Down A.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end
 battTS_Dchrg_1 = dischargeToSOC(0, cRates(1, 1)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
-battTS_Chrg_1 = chargeToSOC(1, cRates(1, 2)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_Dchrg_1", '-append');
 
-save(dataLocation + "001Cycle3Times_"+dateStr,...
-    "battTS_Dchrg_1", "battTS_Chrg_1", '-append');
+% Wait A
+msg = newline + "Cycle 1: Waiting for "+ waitTime + " seconds. NextUp: Cycle 1 Charge.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end    
+battTS_wait_1A = waitTillTime(waitTime, 'cellIDs', cellIDs,...
+    'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_wait_1A", '-append');
+
+% Charge
+msg = newline + "Cycle 1: Charging to 100%. NextUp: Cycle 1 Cool Down B";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end
+battTS_Chrg_1 = chargeToSOC(1, cRates(1, 2)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_Chrg_1", '-append');
+
+% Wait B
+msg = newline + "Cycle 1: Waiting for "+ waitTime + " seconds. NextUp: Cycle 2.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end    
+battTS_wait_1B = waitTillTime(waitTime, 'cellIDs', cellIDs,...
+    'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_wait_1B", '-append');
 
 %% Cycle 2
+% Discharge Stage
+msg = newline + "Cycle 2: Discharging to 0%. NextUp: Cycle 2 Cool Down A.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end
 battTS_Dchrg_2 = dischargeToSOC(0, cRates(2, 1)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
-battTS_Chrg_2 = chargeToSOC(1, cRates(2, 2)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_Dchrg_2", '-append');
 
-save(dataLocation + "001Cycle3Times_"+dateStr,...
-    "battTS_Dchrg_2", "battTS_Chrg_2", '-append');
+% Waiting Stage A
+msg = newline + "Cycle 2: Waiting for "+ waitTime + " seconds. NextUp: Cycle 2 Charge.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end    
+battTS_wait_2A = waitTillTime(waitTime, 'cellIDs', cellIDs,...
+    'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_wait_2A", '-append');
+
+% Charge Stage
+msg = newline + "Cycle 2: Charging to 100%. NextUp: Cycle 2 Cool Down B.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end
+battTS_Chrg_2 = chargeToSOC(1, cRates(2, 2)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_Chrg_2", '-append');
+
+% Waiting Stage B
+msg = newline + "Cycle 2: Waiting for "+ waitTime + " seconds. NextUp: Cycle 3.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end    
+battTS_wait_2B = waitTillTime(waitTime, 'cellIDs', cellIDs,...
+    'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_wait_2B", '-append');
+
 
 %% Cycle 3
+% Discharge Stage
+msg = newline + "Cycle 3: Discharging to 0%. NextUp: Cycle 3 Cool Down A.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end
 battTS_Dchrg_3 = dischargeToSOC(0, cRates(3, 1)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
-battTS_Chrg_3 = chargeToSOC(1, cRates(3, 2)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_Dchrg_3", '-append');
 
-save(dataLocation + "001Cycle3Times_"+dateStr,...
-    "battTS_Dchrg_3", "battTS_Chrg_3", '-append');
+% Waiting Stage
+msg = newline + "Cycle 3: Waiting for "+ waitTime + " seconds. NextUp: Cycle 3 Charge.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end    
+battTS_wait_3A = waitTillTime(waitTime, 'cellIDs', cellIDs,...
+    'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_wait_3A", '-append');
+
+% Charge Stage
+msg = newline + "Cycle 3: Charging to 100%. NextUp: Cycle 3 Cool Down B";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end
+battTS_Chrg_3 = chargeToSOC(1, cRates(3, 2)*CAP, 'cellIDs', cellIDs, 'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_Chrg_3", '-append');
+
+% Waiting Stage
+msg = newline + "Cycle 3: Waiting for "+ waitTime + " seconds. NextUp: End of Cycle.";
+if strcmpi(caller, "gui"), send(randQ, msg); else, disp(msg);end    
+battTS_wait_3B = waitTillTime(waitTime, 'cellIDs', cellIDs,...
+    'testSettings', testSettings);
+save(dataLocation + "001Cycle3Times_"+dateStr, "battTS_wait_3B", '-append');
+
+% save(dataLocation + "001Cycle3Times_"+dateStr,...
+%     "battTS_Dchrg_3", "battTS_Chrg_3", '-append');
 
 metadata.endDate = string(datetime('now', 'Format','yyMMdd'));
 metadata.endTime = string(datetime('now', 'Format','HHmm'));
